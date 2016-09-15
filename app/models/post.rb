dca86072ab9cc68a358403343014a3428899ac70
Chip Carnes and Sebastian Prokuski <chip+sebastian@>
2016-09-15 16:52:40 -0700

    add path to post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,6 @@ class Post < ApplicationRecord
   validates_presence_of :caption, :location, :image
   belongs_to :user
   has_and_belongs_to_many :accounts
-  has_attached_file :image, styles: { :medium => "640>", :thumb => "200#" }
+  has_attached_file :image, styles: { :medium => "640>", :thumb => "200#" }, :path => "flickr-clone-rails/:attachment/:style/:id.:extension"
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 end
